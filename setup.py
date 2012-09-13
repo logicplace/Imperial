6695f52db7ce73d8f1a8d5f27581d515b8b4da58
Wa <admin@logicplace.com>
2012-09-13 13:36:40 -0700

    Added setup.py for the RPL library.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+	name        = "RPL",
+	version     = "0.9d",
+	description = "Library to handle resource replacements. Part of Imperial Exchange.",
+	author      = "Wa",
+	url         = "http://github.com/logicplace/Imperial",
+	packages    = ["rpl"],
+)
