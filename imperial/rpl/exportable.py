ba7d806b9944c5fa7a5ab247687eb0a3191457bd
Sapphire Becker <github@logicplace.com>
2018-07-19 14:40:47 -0700

    Adopting Mozilla Public License 2.0 for this * This is retroactive for the changed files. * They were labeled as MIT before because that's what my snippit does but they're not okay?? * Except mixed in helper.py that's still MIT ** frozendict is public domain ** I'll make this clearer at some point.

--- imperial/rpl/exportable.py
+++ imperial/rpl/exportable.py
@@ -1,7 +1,9 @@
 #-*- coding:utf-8 -*-
 
 # Copyright 2018 Sapphire Becker (logicplace.com)
-# MIT Licensed
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 """
 # exportable #
